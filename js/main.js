d94d39ed120680401e42947a3b3e97a917e10292
david cortesi <david.cortesi.097@gmail.com>
2020-10-26 15:31:49 +0100

    commit 1

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(".header-right a i.fas").click(function() {
+  $(".hamburger-menu").css("display" , "block");
+});
+
+$(".hamburger-menu a i.fas").click(function() {
+  $(".hamburger-menu").css("display" , "none");
+});
